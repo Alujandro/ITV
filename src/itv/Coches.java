3f0eea20ab55fc50c7b341f2e131e55a60e2e378
ciclost <ciclost@ciclost-VirtualBox>
2021-03-02 19:24:55 +0100

    Creados Box, coches y añadido los gets()

---
+++ src/itv/Coches.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package itv;
+
+/**
+ *
+ * @author ciclost
+ */
+public class Coches {
+    //Atributos
+    private String matricula;
+    private String modelo;
+    private String tipo;
+    
+    //Constructores
+    public Coches(String ma, String mo, String ti){
+        this.matricula=ma;
+        this.modelo=mo;
+        this.tipo=ti;
+    }
+    
+    //Métodos
+    public String getMatricula(){
+        return this.matricula;
+    }
+    
+    public String getModelo(){
+        return this.modelo;
+    }
+    
+    public String getTipo(){
+        return this.tipo;
+    }
+    
+    @Override
+    public String toString(){
+        return "Matrícula: "+this.matricula+" Modelo: "+this.modelo+" tipo: "+this.tipo;
+    }
+    
+}
