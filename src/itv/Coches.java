c99d4d3a97bd726f02fae8a7d2d9ba44f364add3
ciclost <ciclost@ciclost-VirtualBox>
2021-03-03 15:57:02 +0100

    Añadidos los sets en Coches, un constructor vacío y el mensaje de entrada de valores

--- src/itv/Coches.java
+++ src/itv/Coches.java
@@ -5,6 +5,10 @@
  */
 package itv;
 
+import java.util.regex.Pattern;
+import java.util.Scanner;
+import itv.util.GestorIO;
+
 /**
  *
  * @author ciclost
@@ -14,6 +18,8 @@ public class Coches {
     private String matricula;
     private String modelo;
     private String tipo;
+    public static final String PATRON="//d{4}[A-Z]{3}";
+    public static final String[] TIPOS={"Microbús","Furgoneta","Camión"};
     
     //Constructores
     public Coches(String ma, String mo, String ti){
@@ -26,6 +32,12 @@ public class Coches {
         this.matricula=co.getMatricula();
         this.modelo=co.getModelo();
         this.tipo=co.getTipo();
+    }
+    
+    public Coches(){
+        this.matricula=null;
+        this.modelo=null;
+        this.tipo=null;
     }
     
     //Métodos
@@ -41,6 +53,45 @@ public class Coches {
         return this.tipo;
     }
     
+    public boolean matriculaValida(String mat){
+        return Pattern.matches(PATRON, mat);
+    }
+    
+    public void setMatricula(String mat){
+        this.matricula=mat;
+    }
+    
+    public void setModelo(String mod){
+        this.modelo=mod;
+    }
+    
+    public void setTipo(int i){
+        this.tipo=TIPOS[i-1];
+    }
+    
+    public void llenaCoche(){
+        Scanner enter=new Scanner(System.in);
+        GestorIO nume=new GestorIO();
+        int num;
+        String entrar=null;
+        System.out.print("Introduce la matrícula: ");
+        while (!matriculaValida(entrar=enter.nextLine())){
+            System.out.print("Intoduzca una matrícula válida: ");
+        }
+        this.setMatricula(entrar);
+        System.out.print("Introduce el modelo: ");
+        this.setModelo(enter.nextLine());
+        System.out.print("Introduce el tipo [1-Microbús, 2-Furgoneta, 3-Camión]: ");
+        while (true){
+            num=nume.inInt();
+            if (num>3 || num<1){
+                System.out.print("Introduce un número [1-3]: ");
+            } else {
+                break;
+            }
+        }
+        this.setTipo(num);
+    }
     @Override
     public String toString(){
         return "Matrícula: "+this.matricula+" Modelo: "+this.modelo+" Tipo: "+this.tipo;
