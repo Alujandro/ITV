1efdf8a5cf07851446a4efd30c827575c990aa73
ciclost <ciclost@ciclost-VirtualBox>
2021-03-05 19:33:38 +0100

    ColaGeneral, el array y las comparaciones funcionan perfectamente, el tamaño de array ahora es una constante

--- src/itv/ColaGeneral.java
+++ src/itv/ColaGeneral.java
@@ -12,28 +12,36 @@ package itv;
 public class ColaGeneral {
     //Atributos
     private final Coches[] cola;
+    private static final int TOTAL=50;
     
     //Constructores
     public ColaGeneral(){
-        this.cola=new Coches[50];
+        this.cola=new Coches[TOTAL];
+        for (int i=0; i<cola.length; i++){
+            this.cola[i]=null;
+        }
     }
     
     //Metodos
     public void avanzar(){
         this.cola[cola.length-1]=null;
-        for (int i=0; i<this.cola.length; i++){
+        for (int i=0; i<this.cola.length-1; i++){
             this.cola[i]=this.cola[i+1];
         }
         System.out.println("La cola ha avanzado de forma satisfactoria");
     }
     
     public Coches getSiguiente(){
+        if (this.cola[0]==null) {
+            return null;
+        }
         return new Coches(this.cola[0]);
     }
     
     public void setCoche(Coches c){
         for (int i=0; cola[i]!=null || i<cola.length; i++){
             this.cola[i]=new Coches(c);
+            System.out.println("Vehículo introducido satisfactoriamente");
             break;
         }
     }
@@ -43,14 +51,14 @@ public class ColaGeneral {
             System.out.println("La cola de entrada está vacía");
             return true;
         } else {
-            if (b.getVehiculo(0)==null){
+            if (b.getVehiculo(0)!=null){
+                System.out.println("No hay hueco en el Box");
+                return false;
+            } else {
                 b.setVehiculoNuevo(this.cola[0]);
                 this.avanzar();
                 System.out.println("Operación realizada con éxito");
                 return true;
-            } else {
-                System.out.println("No hay hueco en el Box");
-                return false;
             }
         }
     }
