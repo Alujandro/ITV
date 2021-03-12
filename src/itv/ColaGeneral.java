b2040ecc7906da6f261e43283a7c83f5d6b33864
ciclost <ciclost@ciclost-VirtualBox>
2021-03-12 18:20:07 +0100

    ColaGeneral, añadido getTotal, mejorado getSiguiente, ahora se compara si matrícula es null en vez de si el objeto coche es null y avanzar funciona correctamente.

--- src/itv/ColaGeneral.java
+++ src/itv/ColaGeneral.java
@@ -18,44 +18,52 @@ public class ColaGeneral {
     public ColaGeneral(){
         this.cola=new Coches[TOTAL];
         for (int i=0; i<cola.length; i++){
-            this.cola[i]=null;
+            this.cola[i]=new Coches();
         }
     }
     
     //Metodos
     public void avanzar(){
-        this.cola[cola.length-1]=null;
         for (int i=0; i<this.cola.length-1; i++){
-            if (this.cola[i]==null){
-                this.cola[i-1]=null;
+            if (this.cola[i].getMatricula()==null){
+                this.cola[i-1]=new Coches();
                 break;
             }
             this.cola[i]=new Coches(this.cola[i+1]);
         }
-        this.cola[TOTAL-1]=null;
+        this.cola[cola.length-1]=new Coches();
     }
     
     public Coches getSiguiente(){
         if (this.cola[0]==null) {
-            return null;
+            return new Coches();
+        }
+        if (this.cola[0].getMatricula()==null){
+            return new Coches();
         }
         return new Coches(this.cola[0]);
     }
     
     public void setCoche(Coches c){
-        for (int i=0; cola[i]!=null || i<cola.length; i++){
-            this.cola[i]=new Coches(c);
-            System.out.println("Vehículo introducido satisfactoriamente");
-            break;
+        for (int i=0;i<cola.length; i++){
+            if (cola[i].getMatricula()==null) {
+                this.cola[i]=new Coches(c);
+                System.out.println("Vehículo introducido satisfactoriamente");
+                break;
+            }
         }
     }
     
+    public int getTOTAL(){
+        return TOTAL;
+    }
+    
     public boolean meterEnBox(Box b){
-        if (this.getSiguiente()==null){
+        if (this.getSiguiente().getMatricula()==null){
             System.out.println("La cola de entrada está vacía");
             return true;
         } else {
-            if (b.getVehiculo(0)!=null){
+            if (b.getVehiculo(0).getMatricula()!=null){
                 System.out.println("No hay hueco en el Box");
                 return false;
             } else {
