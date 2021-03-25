c98f17ebc5deddb285e5d64a2bb6d98be13f13f9
ciclost <ciclost@ciclost-VirtualBox>
2021-03-25 19:25:23 +0100

    Creado VehiculoDeCarga y Camion, ambos tienen el método calculaCoste, aunque todavía no tienen un constructor en VehiculoDeCarga

---
+++ src/itv/vehiculos/Camion.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package itv.vehiculos;
+
+/**
+ *
+ * @author ciclost
+ */
+public class Camion extends VehiculoDeCarga{
+    private static final double FIJO=40;
+    
+    @Override
+    public double calculaCoste(){
+        double resultado;
+        if (this.getCilindros()>=10){
+            resultado=4*this.getPMA();
+        } else {
+            resultado=3*this.getPMA();
+        }
+        if (this.getCentimetrosCubicos()>2000){
+            resultado+=20;
+        }
+        return resultado + FIJO + (this.getCilindros()*15)*1.2;
+    }
+}
